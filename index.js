f7881f05814d3d0698923725a260f13842769cd7
s-lyon2024 <111667797+s-lyon2024@users.noreply.github.com>
2022-11-09 13:09:01 -0500

    firebase practice

--- index.js
+++ index.js
@@ -1,52 +1,52 @@
 // Import stylesheets
-import "./style.css";
+import './style.css';
 
 // Import javascript files
-import "./js/add-element.js";
+import './js/add-element.js';
 
 // Firebase App (the core Firebase SDK) is always required and must be listed first
-import * as firebase from "firebase/app";
+import * as firebase from 'firebase/app';
 
 // Add the Firebase products that you want to use
-import "firebase/auth";
-import "firebase/firestore";
+import 'firebase/auth';
+import 'firebase/firestore';
 
-import * as firebaseui from "firebaseui";
+import * as firebaseui from 'firebaseui';
 
 //  Code for later database connection Please ignore.
 
 // Add Firebase project configuration object here
 var firebaseConfig = {
-  apiKey: "AIzaSyAzcwgZuLA7dO9g4sQhXQVTUgCo0M8m2qM",
-  authDomain: "grocerylist-91956.firebaseapp.com",
-  databaseURL: "https://grocerylist-91956.firebaseio.com",
-  projectId: "grocerylist-91956",
-  storageBucket: "grocerylist-91956.appspot.com",
-  messagingSenderId: "813812426276",
-  appId: "1:813812426276:web:93e5897af12892ff78dab1",
-  measurementId: "G-VZ83BTR72T"
+  apiKey: 'AIzaSyC9IFN_gsJbH-vsNw2bghWq5Shp3zMO3y8',
+  authDomain: 'grocies-700b7.firebaseapp.com',
+  projectId: 'grocies-700b7',
+  storageBucket: 'grocies-700b7.appspot.com',
+  messagingSenderId: '616117787461',
+  appId: '1:616117787461:web:8ec17f04f27cfe51fbfc2c',
+  measurementId: 'G-KVEGFW5T0Q',
 };
 
 firebase.initializeApp(firebaseConfig);
 
 // Save the list to database
-$("#save").click(function() {
-// document.querySelectorAll('li') ==> nodelist
-// for loop to go through each nodelist
-// and then get the text content
-
-  $('li').each(function(){
+$('#save').click(function () {
+  // document.querySelectorAll('li') ==> nodelist
+  // for loop to go through each nodelist
+  // and then get the text content
+  $(this).text('saved');
+  $('li').each(function () {
     var value = $(this).text();
     console.log(value);
 
-    firebase
-    .firestore()
-    .collection("mylist")
-    .add({
-      item: value//?
+    firebase.firestore().collection('mylist').add({
+      item: value, //?
     });
 
-
+    //practice new collection
+    /* 
+    firebase.firestore().collection('mynamelist').add({
+      name: value, //?
+    });
+*/
   });
-
 });
